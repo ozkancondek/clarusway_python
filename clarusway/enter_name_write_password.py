02e6a31ecda1f24a9af170faef5ee27fe27729dc
ozkan <ozkncndekk@gmail.com>
2021-07-13 00:39:26 +0200

    Enter name added

---
+++ clarusway/enter_name_write_password.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 15:15:02 2021
+
+@author: admin
+"""
+password = 3507
+
+name = input("Enter your name: ")
+
+real_name = "ozkan"
+
+if name == real_name:
+    print(f"Hello {real_name}. The password is {password}. ")
+
+else:
+    print(f"Hello {name}. See you later.")
+
