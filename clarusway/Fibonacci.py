9fc88f175427d1aaef127aa5dc97a9163c95e32a
ozkan <ozkncndek@gmail.com>
2021-07-20 20:14:53 +0200

    Fibonacci added

---
+++ clarusway/Fibonacci.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 20 19:15:49 2021
+
+@author: admin
+"""
+
+fibonacci = [0,1]
+
+ 
+for i in range(20): 
+    a = fibonacci[i] + fibonacci[i+1]
+    fibonacci.append(a)
+    i += 1
+    if a > 50:
+        break
+       
+print (fibonacci)
