f7bf0cf2f44770b45435cbe28c80e233d4090212
ozkan <ozkncndek@gmail.com>
2021-07-29 15:05:52 +0200

    lambda_examples added

---
+++ clarusway/lambda_examples.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 29 00:44:20 2021
+
+@author: admin
+"""
+
+  
+#lambda examples
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
