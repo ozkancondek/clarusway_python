59dbedb750b3ef8821df4ccaaddf8512d560c410
ozkancondek <ozkncndek@gmail.com>
2021-08-05 16:06:31 +0200

    Cube example modified

--- my_projects/draw_a_cube.py
+++ my_projects/draw_a_cube.py
@@ -5,8 +5,13 @@ Created on Thu Aug  5 15:18:34 2021
 @author: admin
 """
 #Get element of the cube and line length from the user and print out a cube.
+# Also print to right side the  features of the cube.
 el = input("Give me the element of the cube: ")
 line = int(input("What is the length of the line: "))
 for i in range(line):
-    print(line*(el+(" ")))
-        
+    if i < line // 2:
+        print(line*(el+(" "))+"|")
+    elif i == line // 2:
+        print(line*(el+(" "))+"|"," ",line,"X",line," Cube")
+    else:
+        print(line*(el+(" "))+"|")
