48c8ea7cdd556861d1fcc52677a2d8ace1a6ccb6
ozkancondek <ozkncndek@gmail.com>
2021-08-06 13:34:36 +0200

    pyramid is modified

--- my_projects/pyramit.py
+++ my_projects/pyramit.py
@@ -4,6 +4,10 @@ Created on Thu Jul 29 20:26:56 2021
 
 @author: admin
 """
+#draw a pyramit with entered variables.
+#get building material from the user
+#get base width for pyramid
+#base width has to be odd number otherwise send user a massage that it has to be odd number.
 
 a = input("Choose the building material: ")
 b = " "
