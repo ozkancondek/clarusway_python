72e205856b0ba4744a287a03e832022c4a7d769e
ozkancondek <ozkncndek@gmail.com>
2021-08-09 14:47:07 +0200

    Splittup_numbers example added

---
+++ clarusway/splitup_numbers.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  9 14:17:18 2021
+
+@author: admin
+"""
+num = int(input("Enter a number"))
+
+
+def splitter(num):
+    if num < 0:
+        num = num * -1
+        ls = list(str(num))
+        ls_1 = []
+        for i in range(len(ls)):
+            a = int(ls[i])*(10**((len(ls)-1)-i))
+            ls_1.append(a)
+        for j in range(len(ls_1)):
+            ls_1[j] = ls_1[j]*-1
+
+        return ls_1
+    else:
+        ls = list(str(num))
+        ls_1 = []
+        for i in range(len(ls)):
+            a = int(ls[i])*(10**((len(ls)-1)-i))
+            ls_1.append(a)
+        return ls_1
+
+
+print(splitter(num))
