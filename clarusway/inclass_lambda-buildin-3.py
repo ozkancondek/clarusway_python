99a4f94d31963ca01d6a7256a1c11468b1a1ddba
ozkancondek <ozkncndek@gmail.com>
2021-08-18 21:42:10 +0200

    modified files

--- clarusway/inclass_lambda-buildin-3.py
+++ clarusway/inclass_lambda-buildin-3.py
@@ -6,3 +6,16 @@ Created on Wed Aug 18 20:19:11 2021
 """
 
  
+def function_generator(n):
+    return lambda x: n(x)
+
+
+myPrint =  function_generator(print)
+myMax = function_generator(max)
+myBool = function_generator(bool)
+mySorted = function_generator(sorted)
+print(myBool(1))
+print(myMax([5,1,7,8])) 
+
+
+ 
