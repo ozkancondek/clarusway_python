bf141433e9f0f0db63740d28d65f85c2b0be2ea5
ozkancondek <ozkncndek@gmail.com>
2021-08-23 13:47:08 +0200

    circle module example added

---
+++ clarusway/modülecircle/circle_call_file.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 13:41:41 2021
+
+@author: admin
+"""
+import circle
+#from circle import circlearea
+#from circle import aroundcircle
+
+a = circle.circlearea(5)
+b = circle.aroundcircle(5)
+print(a)
+print(b)
