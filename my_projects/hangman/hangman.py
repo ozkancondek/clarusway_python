0857b022c57e449d60dee91ef0744aaa221d14b9
ozkancondek <ozkncndek@gmail.com>
2021-08-31 00:40:45 +0200

    hangman modified

--- my_projects/hangman/hangman.py
+++ my_projects/hangman/hangman.py
@@ -10,15 +10,26 @@ from visual_dictionary import lives_visual_dict
  
 
 word = "ozkan"
+ls = []
 
-def hangman():
-    for i in range(len(lives_visual_dict)):
-        ch = input("Give me a letter: ")
-        if ch in word:
-            print("Correct go on")
-            ch = input("Give me a letter: ")
+for i in range(1,len(lives_visual_dict)+1):
+     ch = input("Give me a letter: ")
+     ls.append(ch)
+     if ch in word:
+        print("Correct go on")
+        word.replace(ch,"") #delete the letter
+        if word in "".join(ls):
+            print("win")
+            break
         else:
-            print(lives_visual_dict[len(lives_visual_dict)-i])
-print(hangman())
+        
+            ch = input("Give me a letter: ")
+     else:
+         result = lives_visual_dict[len(lives_visual_dict)-i]
+         if result == lives_visual_dict[0]:
+             print(lives_visual_dict[0])
+             print("you losed")
+             break
+ 
             
             
---
+++ my_projects/hangman/trial_file.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 23:43:49 2021
+
+@author: admin
+"""
+
+
+from visual_dictionary import lives_visual_dict
+print(lives_visual_dict[5])
