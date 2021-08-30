8085abbbe227c743492dce42e5db14be54292906
ozkancondek <ozkncndek@gmail.com>
2021-08-30 23:46:13 +0200

    hangman modified

---
+++ my_projects/hangman/hangman.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 23:25:35 2021
+
+@author: admin
+"""
+
+import random
+from visual_dictionary import lives_visual_dict
+import string
+
+word = "ozkan"
+
+def hangman():
+    while True:
+        if 
