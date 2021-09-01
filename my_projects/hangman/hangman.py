a74904bdd376a0cd09c0f47fa0d73cc6f7400c50
ozkancondek <ozkncndek@gmail.com>
2021-09-02 00:31:33 +0200

    hangman last version

--- my_projects/hangman/hangman.py
+++ my_projects/hangman/hangman.py
@@ -9,27 +9,38 @@ Created on Mon Aug 30 23:25:35 2021
 from visual_dictionary import lives_visual_dict
  
 
-word = "ozkan"
-ls = []
-
-for i in range(1,len(lives_visual_dict)+1):
-     ch = input("Give me a letter: ")
-     ls.append(ch)
-     if ch in word:
-        print("Correct go on")
-        word.replace(ch,"") #delete the letter
-        if word in "".join(ls):
-            print("win")
-            break
-        else:
-        
-            ch = input("Give me a letter: ")
-     else:
-         result = lives_visual_dict[len(lives_visual_dict)-i]
-         if result == lives_visual_dict[0]:
-             print(lives_visual_dict[0])
-             print("you losed") 
-             break
+word = "aabbccdde" # Pick a word at the beginning.
  
+c = 1
+ 
+print("Welcome to HANGMAN-GAME.\nYou need to enter a letter!\nNote:Dont type a letter twice.It will be counted as a mistake. ")
+while c <= len(lives_visual_dict)+1:
+     ch = input("Type a letter: ")
+     
+     if ch in word:
+        print(f"Correct! {ch} is in word {word.count(ch)} times.")
+         
+        word = word.replace(ch,"",word.count(ch)) #delete the letters
+         
+        c -= 1
+      
+        if word == "":
+            print("You win!!")
+            break
+     
+         
+     else:
+         result = lives_visual_dict[len(lives_visual_dict)-c]
+         print(result)
+         
+         if result == lives_visual_dict[0]:
+              
+             print("You losed!!!") 
+             break
+     c+=1
+      
             
+        
             
+        
+        
