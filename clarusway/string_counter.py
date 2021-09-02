8de1ea4f797fc859e8ecd67215cd781190cbad3e
ozkancondek <ozkncndek@gmail.com>
2021-09-02 19:32:46 +0200

    string counter example added

---
+++ clarusway/string_counter.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  2 19:09:25 2021
+
+@author: admin
+"""
+#Count the strings
+
+string = "What a lovely day!"
+
+dict_word = {}
+for n in string:
+    keys = dict_word.keys()
+    if n in dict_word:
+        dict_word[n] +=1
+    else:
+        dict_word[n] = 1
+        
+print(dict_word)
+    
