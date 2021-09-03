80ca06084e32dd688774415f4e8e206c98dfbed6
ozkancondek <ozkncndek@gmail.com>
2021-09-03 20:52:43 +0200

    snail last version

--- my_projects/snail_matrix/simplifier.py
+++ my_projects/snail_matrix/simplifier.py
@@ -5,14 +5,19 @@ Created on Fri Sep  3 17:21:00 2021
 @author: admin
 """
  
-ls = [1, 2, 3, 3, 6, 9]
+ls = [1, 2, 3, 3, 6, 9,1]
 
 def simplifier(ls):  
-    for i in ls:
-        c = ls.count(i)
-        if c != 1:
-            ls.remove(i)
+     for i in ls:
+       a = ls.count(i)
+       if a != 1:
+           ls.reverse()
+           ls.remove(i)
+           ls.reverse()
+       
+       
             
-    return ls
+     return ls
 
+ 
  
--- my_projects/snail_matrix/snail_function.py
+++ my_projects/snail_matrix/snail_function.py
@@ -6,20 +6,40 @@ Created on Fri Sep  3 17:10:05 2021
 """
 
 from matrix_list_function import matrix
+from simplifier import simplifier
+ 
+def snail(n):
 
-n = 3
-mtr = matrix(n)
-ls = []
+    mtr = matrix(n)
+    ls = []
+    for I in range(n//2):
+        for j in range(len(mtr)):
+            ls.append(mtr[0][j])
+        for i in range(len(mtr)):
+            ls.append(mtr[i][len(mtr)-1])
+        for x in range(len(mtr)-1,-1,-1):
+            ls.append(mtr[len(mtr)-1][x])
+        for y in range(len(mtr)-1,-1,-1):
+            ls.append(mtr[y][0])
+         
+        for m in mtr:
+            
+                m.remove(m[len(m)-1])
+            
+        for q in mtr:
+            
+                q.remove(q[0])
+            
+        mtr.remove(mtr[0])
+        
+        mtr.remove(mtr[len(mtr)-1]) 
+         
+    if n%2 != 0:
+        ls.append(mtr[0][0])
+    return simplifier(ls)
 
-for j in range(n):
-    ls.append(mtr[0][j])
-for i in range(n):
-    ls.append(mtr[i][n-1])
-for x in range(n-1,-1,-1):
-    ls.append(mtr[n-1][x])
-for y in range(n-1,-1,-1):
-    ls.append(mtr[y][0])
-
-print(ls)
+ 
+    
+ 
 
 
