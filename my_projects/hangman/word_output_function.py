c4684d7a9041f36d374e1d0bac9bc25f11d5ff06
ozkancondek <ozkncndek@gmail.com>
2021-09-16 21:35:29 +0200

    word output function added

---
+++ my_projects/hangman/word_output_function.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 01:24:35 2021
+
+@author: admin
+"""
+ 
+
+
+def wor(word,ch):
+    ls = list(word)
+    print(ls)
+    c = 0
+    while c < len(ls):
+        if ls[c] != ch:
+            ls[c] = "_ "
+        c += 1
+    return "".join(ls)
+
+ 
+            
+     
+    
+    
